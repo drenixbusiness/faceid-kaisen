51991d27c903b8e194cebe296b94c2d492138a92
drenixbusiness <drenixit@gmail.com>
2026-04-25 02:25:37 +0500

    fix ecosystem - pass only parsed .env vars to PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,13 @@
+const dotenv = require('dotenv');
+const path = require('path');
+
+const env = dotenv.config({ path: path.join(__dirname, '.env') }).parsed || {};
+
 module.exports = {
   apps: [{
     name: 'faceid-kaisen',
     script: './app.js',
-    cwd: '/home/faceid/faceid-kaisen'
+    cwd: __dirname,
+    env
   }]
 };
